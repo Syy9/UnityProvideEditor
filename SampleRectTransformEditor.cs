8a79851aedd0f499e0896a85408ca8b7643ad7a7
Syy <shirakawa.yy@gmail.com>
2018-10-29 23:35:40 +0900

    Add sample editor

---
+++ SampleRectTransformEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CustomEditorUtil;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RectTransform))]
+public class SampleRectTransformEditor : UnityProvideEditor
+{
+    protected override UnityProvideEditorType EditorType { get { return UnityProvideEditorType.RectTransformEditor; } }
+
+    RectTransform Target;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        Target = (RectTransform) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Sample"))
+        {
+            Debug.Log("Click! " + Target.name);
+        }
+    }
+}
